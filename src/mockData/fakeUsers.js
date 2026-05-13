efa603fdf4b4bc3e4992e53a610d890944acb735
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-05-13 15:07:42 +0700

    added backend

---
+++ src/mockData/fakeUsers.js
@@ -0,0 +1,18 @@
+export const users = [{ 
+    id: "1", 
+    username: "Neeti",
+    email: "neeti@example.com", 
+    password: "pass123",
+},
+{
+    id: "2",
+    username: "Somchai",
+    email: "somchai@example.com",
+    password: "pass456",
+},
+{
+    id: "3",
+    username: "Anya",
+    email: "anya@example.com",
+    password: "pass789",
+}]
