1a22b6d9017fe41e748153510d167405efed805a
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-05-21 13:13:32 +0700

    added auth

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -7,7 +7,13 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, minlength: 8, select: false },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
+
+//Hash password before saving
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 12);
+});
 
 export const User = mongoose.model("User", userSchema);
