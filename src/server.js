0887c713136b3a4a06bd3c58ab75e533c86b6a42
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-05-25 10:36:36 +0700

    added login

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ import { connectSupabase } from "./config/supabase.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser()); // ✅ Enable cookie parsing
